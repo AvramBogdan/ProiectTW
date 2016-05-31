0377d236892b0a15725e2168574dad7781c7306d
florinaiancu <florinaiancu5@gmail.com>
2016-05-31 17:01:30 +0300

    Update routes.php

--- ProiectTW/app/Http/routes.php
+++ ProiectTW/app/Http/routes.php
@@ -10,6 +10,28 @@
 | and give it the controller to call when that URI is requested.
 |
 */
+Route::get('/', function () {
+    return view('welcome');
+});
+Route::get('home',function(){
+	if(Auth::guest()){
+		return Redirect::to('auth/login');
+	}
+	
+});
+
+// Authentication routes...
+Route::get('auth/login', 'Auth\AuthController@getLogin');
+Route::post('auth/login', 'Auth\AuthController@postLogin');
+Route::get('auth/logout', 'Auth\AuthController@getLogout');
+
+// Registration routes...
+Route::get('auth/register', 'Auth\AuthController@getRegister');
+Route::post('auth/register', 'Auth\AuthController@postRegister');
+
+Route::auth();
+
+Route::get('/home', 'HomeController@index');
 
 Route::get('Contact', 'ControlPagini@getContacts');
 
