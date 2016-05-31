8c64d55b96824b688bae2db2c0b3a30f8d8e1485
basescuandrei <basescu_c@yahoo.com>
2016-05-31 13:50:25 +0000

    Add files via upload

---
+++ ProiectTW/resources/views/top.blade.php
@@ -0,0 +1,254 @@
+<html>
+  <head>
+    <title> Top </title>
+	<link rel="stylesheet" type="text/css" href="css/top_css.css" />
+  
+    <link rel="stylesheet" href=css/HomeStyle.css type="text/css"> 
+  </head>
+  <body background ="css/img.jpg">  
+
+    <HEADER> 
+
+        <H1>  TRAVLR </H1>
+
+    </HEADER>
+
+
+
+    <a href =# class="button"> Home </a> 
+    <a href =# class="button"> Preferences </a> 
+    <a href =# class="button"> Routes </a> 
+    <a href =# class="button"> Info </a> 
+    <a href =# class="button"> About </a> 
+    <a href =# class="button"> Contact Us </a>
+  <div >
+    
+    <h4>1. Cathay Pacific Airways</h4>
+	<ul>
+    <li>Țară: China, Hong-Kong</li>
+    <li>Cod: CX, CPA</li>	
+	</ul>
+	
+	<p>Compania a fost fondată în Shanghai, în anul 1946, de către americanul Roy C.
+	Farrell și australianul Sydney H. de Kantzow. Inițial, erau transportați pasageri doar 
+	către Manila, Bangkok, Singapore și Shanghai, însă compania s-a extins rapid. La 68 de
+	ani de la înființare, Cathay Pacific a fost desemnată de Skytrax drept „Cea mai Bună 
+	Companie Aeriană din Lume”, pentru a patra oară, mai mult decât oricare altă companie 
+	din lume. În prezent, Cathay Pacific are aproape 33.000 angajați în întreaga lume și deține 
+	o flotă de 146 avioane.</p>
+	<form action="adaugare_nota.php" method="POST">
+     Dati o nota :
+	 <input type="radio" name="not1" value="1">
+	 <input type="radio" name="not1" value="2">
+	 <input type="radio" name="not1" value="3">
+	 <input type="radio" name="not1" value="4">
+	 <input type="radio" name="not1" value="5">
+
+     <br /><input type="submit" name="submit" value="Trimite Nota" />
+     </form> 
+	
+	</div>
+	
+	<div>
+	    <h4>2. Emirates</h4>
+	<ul>
+    <li>Țară: Emiratele Arabe Unite</li>
+    <li>Cod: EK, UAE</li>	
+	</ul>
+	<p>Cea mai mare companie aeriană din Orientul Mijlociu și-a început activitatea în 
+	anul 1985. Câștigătoare a numeroase premii internaționale, compania deținută în
+	totalitate de guvernul din Dubai și-a construit reputația datorită calității serviciilor.
+	În prezent, Emirates deține o flotă de circa 230 avioane, transportând pasageri în mai bine 
+	de 140 de destinații din 80 țări.	În anul financiar 2013/2014, Emirates a transportat 44,5 milioane pasageri
+	și 2,25 milioane de tone de marfă.
+	</p>
+	<form action="adaugare_nota.php" method="POST">
+     Nota: 
+	 <input type="radio" name="not2" value="1">
+	 <input type="radio" name="not2" value="2">
+	 <input type="radio" name="not2" value="3">
+	 <input type="radio" name="not2" value="4">
+	 <input type="radio" name="not2" value="5">
+	 
+     <br /><input type="submit" name="submit" value="Trimite Nota" />
+     </form> 
+	
+	</div>
+	<div>
+	 <h4>3. EVA Air</h4>
+	<ul>
+    <li>Țară: Taiwan</li>
+    <li>Cod: BR, EVA</li>	
+	</ul>
+	<p>EVA Air este prima linie aeriană privată din Taiwan, fondată în 1989 de către omul de afaceri 
+	Chang Yung-Fa, deținător al Grupului Evergreen. Acesta a atras atenția comunității internaționale 
+	cu o investiție inițială de 3,6 miliarde dolari, bani cu care a achiziționat 26 avioane. În prezent,
+	flota este formată din 61 avioane. Unul dintre cele mai mari motive de mândrie ale companiei este că, 
+	în toți anii de existență, niciun avion nu a înregistrat defecțiuni majore sau accidente.</p>
+	<form action="adaugare_nota.php" method="POST">
+     Nota: 
+	 <input type="radio" name="not3" value="1">
+	 <input type="radio" name="not3" value="2">
+	 <input type="radio" name="not3" value="3">
+	 <input type="radio" name="not3" value="4">
+	 <input type="radio" name="not3" value="5">
+	 
+     <br /><input type="submit" name="submit" value="Trimite Nota" />
+	</div>
+	<div>
+		 <h4>4.Air Canada</h4>
+	<ul>
+    <li>Țară: Canada</li>
+    <li>Cod: AC, ACA</li>	
+	</ul>
+	<p>Denumită inițial Trans-Canada Air Lines (CTA), cea mai mare companie aeriană din Canada și-a
+	început activitatea în anul 1937, cu un zbor de 50 minute (avionul transporta doi pasageri și corespondență). 
+	În prezent, compania cu sediul central în Montreal are 20.000 angajați în întreaga lume și, împreună cu linia Express, 
+	transportă anual aproximativ 35 milioane pasageri, în circa 180 de destinații, de pe cinci continente.</p>
+	<form action="adaugare_nota.php" method="POST">
+     Nota: 
+	 <input type="radio" name="not4" value="1">
+	 <input type="radio" name="not4" value="2">
+	 <input type="radio" name="not4" value="3">
+	 <input type="radio" name="not4" value="4">
+	 <input type="radio" name="not4" value="5">
+	 
+     <br /><input type="submit" name="submit" value="Trimite Nota" />
+	</div>
+	<div>
+			 <h4>5. KLM</h4>
+	<ul>
+    <li>Țară: Olanda</li>
+    <li>Cod: KL, KLM</li>	
+	</ul>
+	<p>Fondată în 1919, la cererea Reginei Wilhelmina, KLM este cea mai veche companie aeriană din lume care operează 
+	în continuare sub numele inițial. Inițialele numelui provin de la Koninklijke Luchtvaart Maatschappij, tradus prin 
+	„linia aeriană regală olandeză”. Cu circa 33.000 angajați în întreaga lume, KLM transportă pasageri și marfă în peste 
+	130 destinații. În afară de dezastrul din Tenerife, din 1977, (în care două avioane s-au ciocnit la decolare, 
+	ducând la decesul a aproape 600 persoane), avioanele KLM nu au fost implicate în accidente cu victime.</p>
+	<form action="adaugare_nota.php" method="POST">
+     Nota: 
+	 <input type="radio" name="not5" value="1">
+	 <input type="radio" name="not5" value="2">
+	 <input type="radio" name="not5" value="3">
+	 <input type="radio" name="not5" value="4">
+	 <input type="radio" name="not5" value="5">
+	 
+     <br /><input type="submit" name="submit" value="Trimite Nota" />
+	</div>
+	
+	<div>
+			 <h4>6. Air New Zealand</h4>
+	<ul>
+    <li>Țară: Noua Zeelandă</li>
+    <li>Cod: NZ, ANZ</li>	
+	</ul>
+	<p>Compania națională de zbor a Noii Zeelande a fost fondată în 1940, iar în prezent transportă pasageri în 15
+	țări din Asia, Europa, America de Nord și Oceania, realizând zboruri către circa 60 de destinații. 
+	Cu o flotă de 105 avioane, Air New Zealand este deținută în proporțiede 53% de guvernul Noii Zeelande, 
+	având peste 11.000 angajați în întreaga lume. Potrivit unui raport din 2013, compania transportă anual 
+	circa 12.000 milioane persoane.</p>
+	<form action="adaugare_nota.php" method="POST">
+     Nota: 
+	 <input type="radio" name="not6" value="1">
+	 <input type="radio" name="not6" value="2">
+	 <input type="radio" name="not6" value="3">
+	 <input type="radio" name="not6" value="4">
+	 <input type="radio" name="not6" value="5">
+	 
+     <br /><input type="submit" name="submit" value="Trimite Nota" />
+	</div>
+	
+	<div>
+		 <h4>7. QANTAS</h4>
+	<ul>
+    <li>Țară: Australia</li>
+    <li>Cod: QF, QFA</li>	
+	</ul>
+	<p>Fondată în 1920, cea mai mare companie aeriană din Australia a fost înregistrată inițial sub numele 
+	de „Queensland and Northern Territory Aerial Services (QANTAS)”. Compania are circa 30.000 angajați,
+	iar 93% dintre aceștia lucrează în Australia. În prezent, flota sa este formată din peste 130 avioane, 
+	ce zboară în 42 de destinații. Ultimul accident soldat cu victime în care a fost implicat un avion al
+	QANTAS s-a petrecut în  anul 1951.</p>
+	<form action="adaugare_nota.php" method="POST">
+     Nota: 
+	 <input type="radio" name="not7" value="1">
+	 <input type="radio" name="not7" value="2">
+	 <input type="radio" name="not7" value="3">
+	 <input type="radio" name="not7" value="4">
+	 <input type="radio" name="not7" value="5">
+	 
+     <br /><input type="submit" name="submit" value="Trimite Nota" />
+	</div>
+	
+	<div>
+		 <h4>8. Hainan Airlines</h4>
+	<ul>
+    <li>Țară: China</li>
+    <li>Cod: HU, CHH</li>	
+	</ul>
+	<p>Cu o experiență de doar 21 ani în transportul de pasageri, Hainan Airlines este cea mai mare companie 
+	aeriană privată din China și una dintre cele opt companii din lume care au primit calificativul de cinci 
+	stele din partea Skytrax (dintre companiile prezente în acest clasament, doar Cathay Pacific a mai primit
+	o avaluare similară). Hainan Airline deține o flotă de 133 avioane și transportă pasageri în circa 90 de 
+	destinații din Asia, Europa, America și Africa.</p>
+	<form action="adaugare_nota.php" method="POST">
+     Nota: 
+	 <input type="radio" name="not8" value="1">
+	 <input type="radio" name="not8" value="2">
+	 <input type="radio" name="not8" value="3">
+	 <input type="radio" name="not8" value="4">
+	 <input type="radio" name="not8" value="5">
+	 
+     <br /><input type="submit" name="submit" value="Trimite Nota" />
+	</div>
+	
+	<div>
+		 <h4>9. JetBlue Airlines</h4>
+	<ul>
+    <li>Țară: Statele Unite ale Americii</li>
+    <li>Cod: B6, JBU</li>	
+	</ul>
+	<p>JetBlue Airlines este o companie low-cost, cu sediul central în New York. JetBlue a fost fondată în 1998, 
+	de antreprenorul David Neeleman, cel care a pus bazele și altor două companii de zbor (Morris Air și Azul
+	Brazilian Airlines). Cu o flotă impresionantă de 204 avioane, JetBlue transportă pasageri cu precădere în 
+	Statele Unite ale Americii, dar și în Caraibe, America de Sud și America Latină, având în total 88 de destinații.</p>
+	<form action="adaugare_nota.php" method="POST">
+     Nota: 
+	 <input type="radio" name="not9" value="1">
+	 <input type="radio" name="not9" value="2">
+	 <input type="radio" name="not9" value="3">
+	 <input type="radio" name="not9" value="4">
+	 <input type="radio" name="not9" value="5">
+	 
+     <br /><input type="submit" name="submit" value="Trimite Nota" />
+	</div>
+	
+	
+	<div>
+		 <h4>10. Etihad Airways</h4>
+	<ul>
+    <li>Țară: Emiratele Arabe Unite</li>
+    <li>Cod: EY, ETD</li>	
+	</ul>
+	<p>Fondată prin decret regal în urmă cu doar 12 ani, Etihad Airways este compania națională de zbor 
+	a Emiratelor Arabe Unite, având sediul în Abu Dhabi. A patra mare companie aeriană din Orientul Mijlociu și a 
+	doua din EAU, Etihad realizează săptămânal circa 1.000 zboruri, în 120 de destinații din întreaga lume. Ritmul
+	de extindere al companiei este unul dintre cele mai rapide din lume: în prima jumătate a anului 2014, 
+	a transportat 6,7 milioane pasageri, în creștere cu 22% față de aceeași perioadă a anului precedent. 
+	Totodată, numărul de kilometri a urcat cu 20%, iar profitul cu aproape 30%.</p>
+	<form action="adaugare_nota.php" method="POST">
+     Nota: 
+	 <input type="radio" name="not10" value="1">
+	 <input type="radio" name="not10" value="2">
+	 <input type="radio" name="not10" value="3">
+	 <input type="radio" name="not10" value="4">
+	 <input type="radio" name="not10" value="5">
+	 
+     <br /><input type="submit" name="submit" value="Trimite Nota" />
+	</div>
+	
+	
+	
+  </body>
+</html>
